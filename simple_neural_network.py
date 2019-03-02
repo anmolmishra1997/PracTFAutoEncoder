880784dde306358f38bbe3e09bd1821c96835b62
Anmol Mishra <anmolmishra1997@gmail.com>
2019-03-02 07:42:34 +0000

    Prac Assignment

---
+++ simple_neural_network.py
@@ -0,0 +1,32 @@
+# import the necessary packages
+from sklearn.preprocessing import LabelEncoder
+from sklearn.cross_validation import train_test_split
+from keras.models import Sequential
+from keras.layers import Activation
+from keras.optimizers import SGD
+from keras.layers import Dense
+from keras.utils import np_utils
+from imutils import paths
+import numpy as np
+import argparse
+import cv2
+import os
+
+def image_to_feature_vector(image, size=(32, 32)):
+	# resize the image to a fixed size, then flatten the image into
+	# a list of raw pixel intensities
+	return cv2.resize(image, size).flatten()
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True,
+	help="path to input dataset")
+args = vars(ap.parse_args())
+
+# grab the list of images that we'll be describing
+print("[INFO] describing images...")
+imagePaths = list(paths.list_images(args["dataset"]))
+
+# initialize the data matrix and labels list
+data = []
+labels = []
